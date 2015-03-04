4db3725cae420f195c9eb77acd5d288b989b413c
Phil Yang <ud1937@gmail.com>
2015-03-05 02:10:13 +0800

    demo done

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	_ "teameditor/routers"
 	"github.com/astaxie/beego"
+	"net/http"
+	"teameditor/controllers"
 )
 
 func main() {
+	go func() {
+		http.HandleFunc("/echo", controllers.EchoServer)
+		err := http.ListenAndServe(":18001", nil)
+		if err != nil {
+			panic("ListenAndServe: " + err.Error())
+		}
+	}()
 	beego.Run()
 }
 
